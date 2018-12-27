6168f3d87db808b96b530b595eab1035d7ed0cb3
Ionut Alazaroae <ionut.alazaroae@hotmail.com>
2018-12-27 02:42:08 +0100

    Menu items modified, fragments and progressbar added, two layouts added for both screen orientations and thread for UI updating

--- app/src/main/java/com/example/ionutpc/cloudmobile/ConsumptionFragment.java
+++ app/src/main/java/com/example/ionutpc/cloudmobile/ConsumptionFragment.java
@@ -1,16 +1,106 @@
 package com.example.ionutpc.cloudmobile;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class ConsumptionFragment extends android.support.v4.app.Fragment {
+
+    private TextView txtProgressCall;
+    private ProgressBar progressBarCall ;
+    private TextView txtProgressText;
+    private ProgressBar progressBarText ;
+    private TextView txtProgressData;
+    private ProgressBar progressBarData;
+    private int callStatus = 0;
+    private int textStatus = 0;
+    private int dataStatus = 0;
+    private Handler handler = new Handler();
+    private int result = 45;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_consumption,container,false);
+
+        View view = inflater.inflate(R.layout.fragment_consumption,container,false);
+        txtProgressCall =  view.findViewById(R.id.txtProgressCall);
+        progressBarCall =  view.findViewById(R.id.progressBarCall);
+        txtProgressText =  view.findViewById(R.id.txtProgressText);
+        progressBarText =  view.findViewById(R.id.progressBarText);
+        txtProgressData =  view.findViewById(R.id.txtProgressData);
+        progressBarData =  view.findViewById(R.id.progressBarData);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (callStatus <= result) {
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            progressBarCall.setProgress(callStatus);
+                            txtProgressCall.setText(callStatus + " %");
+                        }
+                    });
+                    try {
+                        Thread.sleep(16);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    callStatus++;
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (textStatus <= result) {
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            progressBarText.setProgress(textStatus);
+                            txtProgressText.setText(textStatus + " %");
+                        }
+                    });
+                    try {
+                        Thread.sleep(16);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    textStatus++;
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (dataStatus <= result) {
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            progressBarData.setProgress(dataStatus);
+                            txtProgressData.setText(dataStatus + " %");
+                        }
+                    });
+                    try {
+                        Thread.sleep(16);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    dataStatus++;
+                }
+            }
+        }).start();
+
+        return view;
+
     }
 }
--- app/src/main/java/com/example/ionutpc/cloudmobile/DashboardActivity.java
+++ app/src/main/java/com/example/ionutpc/cloudmobile/DashboardActivity.java
@@ -8,16 +8,17 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
+
 public class DashboardActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
-    private DrawerLayout drawer;
+    private  DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
@@ -34,20 +35,22 @@ public class DashboardActivity extends AppCompatActivity  implements NavigationV
 
     }
 
+
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()){
             case R.id.nav_dashboard:
+                Log.d("ionut test","getting in fragment");
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ConsumptionFragment()).commit();
+
                 break;
             case R.id.nav_faq:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FAQFragment()).commit();
                 break;
         }
 
-
         drawer.closeDrawer(GravityCompat.START);
-
         return true;
     }
 
