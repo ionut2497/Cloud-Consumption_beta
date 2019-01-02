97a5ddba36802aa731e9605a3f615bfa4320e093
Ionut Alazaroae <ionut.alazaroae@hotmail.com>
2019-01-02 19:54:45 +0100

    Added noInternet Support for the app

---
+++ app/src/main/java/com/example/ionutpc/cloudmobile/noInternetFragment.java
@@ -0,0 +1,67 @@
+package com.example.ionutpc.cloudmobile;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class noInternetFragment extends  android.support.v4.app.Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_no_internet, container, false);
+        new CheckInternet().execute();
+        return view;
+
+    }
+
+
+    public class CheckInternet extends AsyncTask<String, Void , String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+
+        @Override
+        protected String doInBackground(String... params) {
+             Boolean noInternet = true;
+
+            while (noInternet) {
+
+                if (((splashActivity) getActivity()).checkInternet()) {
+                    noInternet = false;
+                    Log.d("onStartTestFrag", "test true");
+                    Intent homeIntent = new Intent(getActivity(), DashboardActivity.class);
+                    startActivity(homeIntent);
+                    getActivity().finish();
+                    return noInternet.toString();
+
+                } else {
+                    Log.d("onStartTestFrag", "test false");
+                    Log.d("onStartTestFrag", String.valueOf(((splashActivity) getActivity()).checkInternet()));
+
+                }
+
+            }
+
+            return noInternet.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String noInternet) {
+            if(!Boolean.valueOf(noInternet)){
+                Intent homeIntent = new Intent(getActivity(), DashboardActivity.class);
+                startActivity(homeIntent);
+                getActivity().finish();
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/example/ionutpc/cloudmobile/splashActivity.java
+++ app/src/main/java/com/example/ionutpc/cloudmobile/splashActivity.java
@@ -1,25 +1,71 @@
 package com.example.ionutpc.cloudmobile;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class splashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 1000;
+    private boolean hasInternet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("onStartTest","onStart triggered "+ String.valueOf(hasInternet));
+        super.onStart();
+        final Context context = getApplicationContext();
+        final Handler handler = new Handler();
+        final Handler checkInternetHandler = new Handler();
+        checkInternetHandler.postDelayed(new Runnable() {
             @Override
-            public  void run(){
-                Intent homeIntent = new Intent(splashActivity.this,DashboardActivity.class);
-                startActivity(homeIntent);
-                finish();
+            public void run() {
+                hasInternet = checkInternet();
+                if(hasInternet){
+                    Intent homeIntent = new Intent(splashActivity.this,DashboardActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+                else{
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_splash,new noInternetFragment()).commit();
+
+
+                        }
+                    });
+                }
             }
         },SPLASH_TIME_OUT);
+
+    }
+
+    public boolean checkInternet() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedMobile || haveConnectedWifi;
     }
 }
